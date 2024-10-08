7649e72ec976e2453d733e555a971b971dbfd207
Felipe Festa <felipe.festa31@gmail.com>
2024-10-08 12:46:30 -0300

    Deploy Flutter Web app to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7bb888cec2dc2a63beccdb0c23ae024d
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "ec6f9426e807c0355c033d3573a69db2",
+"flutter_bootstrap.js": "8d34a8a4b6968adc606baef3971e2494",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
